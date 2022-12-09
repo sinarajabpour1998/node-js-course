4956414bc70417669a86399367dac75d215777dd
Sina Rajabpour <sina.rajabpour@outlook.com>
2022-12-09 23:23:18 +0330

    Added routes with node.js

--- Routing/index.js
+++ Routing/index.js
@@ -3,7 +3,19 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is overview');
+    } else if(pathName === '/product') {
+        res.end('This is product!');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'test-header': 'test-header'
+        });
+        res.end('<h1>404 , page not found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
